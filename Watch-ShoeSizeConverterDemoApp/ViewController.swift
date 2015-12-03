c91f67f0c883bd8a511521201b389deb988c8744
ElijahButers <Elijahbuters@gmail.com>
2015-12-03 20:12:44 +0200

    Initial Commit

---
+++ Watch-ShoeSizeConverterDemoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Watch-ShoeSizeConverterDemoApp
+//
+//  Created by User on 12/3/15.
+//  Copyright © 2015 Elijah Buters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
